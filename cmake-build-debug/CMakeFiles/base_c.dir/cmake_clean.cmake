d0539ca4c2c33361f07e89ae2afead948ff5c6fa
jonas <jonas_von@163.com>
2020-11-25 00:05:45 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/base_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/base_c.dir/main.cpp.obj"
+  "base_c.exe"
+  "base_c.exe.manifest"
+  "base_c.lib"
+  "base_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/base_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
